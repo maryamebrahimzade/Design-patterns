2c42e8e1c0876740d937f20ec9c9244346e3605e
maryamebrahimzade <ma.ebrahimzadeh@gmail.com>
2023-08-09 14:26:55 +0330

    add->copydeepandshallow

---
+++ src/main/java/org/mapsa/cretional/prototype/copydeepandshallow/Car.java
@@ -0,0 +1,14 @@
+package org.mapsa.cretional.prototype.copydeepandshallow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    public String name;
+    public List<String> colors;
+    public Car(String name, List<String> colors)
+    {
+        this.name = name;
+        this.colors = colors;
+    }
+}
---
+++ src/main/java/org/mapsa/cretional/prototype/copydeepandshallow/Main.java
@@ -0,0 +1,38 @@
+package org.mapsa.cretional.prototype.copydeepandshallow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a Honda car object
+        List<String> hondaColors = new ArrayList<>();
+        hondaColors.add("Red");
+        hondaColors.add("Blue");
+        Car honda=new Car("Honda",hondaColors);
+
+        // Deep copy of Honda
+        Car deepCopyHonda=new Car(honda.name,new ArrayList<>(honda.colors));
+        deepCopyHonda.colors.add("Green");
+        System.out.print("Deep Copy: ");
+        for(String color:deepCopyHonda.colors){
+            System.out.print(color+ ",");
+        }
+        System.out.print("\nOriginal: ");
+        for(String color:honda.colors){
+            System.out.print(color+",");
+        }
+        System.out.println("\n");
+        // Shallow Copy of Honda
+        Car copyHonda=honda;
+        copyHonda.colors.add("Green");
+        System.out.print("Shallow Copy: ");
+        for(String color:copyHonda.colors){
+            System.out.print(color+ ",");
+        }
+        System.out.print("\nOriginal: ");
+        for(String color:honda.colors){
+            System.out.print(color+",");
+        }
+    }
+}
